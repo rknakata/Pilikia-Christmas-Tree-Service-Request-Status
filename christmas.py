cd759773fe94f0ba1f452a2586e01828da9fbc87
Ryan Nakata <rknakata@hawaii.edu>
2019-10-08 15:38:30 -1000

    Update christmas.py
    add notes

--- christmas.py
+++ christmas.py
@@ -1,5 +1,20 @@
+# when there are no service requests available the website displays
+# "No ResNet Pilikia are currently open. Please check again later."
+# <p style="font-size:18px">No ResNet Pilikia are currently open. Please check again later.</p>
+# twill
+# https://github.com/ctb/twill/blob/master/doc/python-api.txt
+# example
+# http://code.activestate.com/recipes/577465-web-testing-using-twill/
+
+# twill documentation site down
+#https://web.archive.org/web/20181114022639/http://twill.idyll.org/
+
+# go <url> -- visit the given URL. The Python function returns the final URL visited, after all redirects.
+
 import time
 import os
+from twill import get_browser
+from twill.commands import *
 
 presents = false #there are no service requests
 
